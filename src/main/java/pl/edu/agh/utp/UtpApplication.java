02620df352d7e40ef92b9282e10c77694c52ab66
Karol Sewiło <409965@student.agh.edu.pl>
2023-12-02 21:48:37 +0100

    initial commit

---
+++ src/main/java/pl/edu/agh/utp/UtpApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtpApplication.class, args);
+	}
+
+}
