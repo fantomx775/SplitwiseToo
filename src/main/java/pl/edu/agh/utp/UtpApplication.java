b8e747b2539fe8f3e2de7c9c19b5fe76d07edbbf
Karol Sewiło <409965@student.agh.edu.pl>
2023-12-02 23:15:10 +0100

    add code formatter

--- src/main/java/pl/edu/agh/utp/UtpApplication.java
+++ src/main/java/pl/edu/agh/utp/UtpApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UtpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UtpApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UtpApplication.class, args);
+  }
 }
