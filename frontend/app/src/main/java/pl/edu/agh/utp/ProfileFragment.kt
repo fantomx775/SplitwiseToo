acc777ccda469cc7407ed7b46b8b8b6595833d6d
Mateusz Marczyk <410191@student.agh.edu.pl>
2023-12-14 21:55:35 +0100

    Profile Fragment

--- frontend/app/src/main/java/pl/edu/agh/utp/ProfileFragment.kt
+++ frontend/app/src/main/java/pl/edu/agh/utp/ProfileFragment.kt
@@ -1,31 +1,38 @@
 package pl.edu.agh.utp
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import pl.edu.agh.utp.manager.UserManager
+import pl.edu.agh.utp.model.User
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_NAME = "name"
+private const val ARG_EMAIL = "email"
+private const val ARG_PASSWORD = "password"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var name: String? = "TestName"
+    private var email: String? = "test@mail.com"
+    private var password: String? = "TestPassword"
+    private var userManager: UserManager? = null
+    private var User: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            name = it.getString(ARG_NAME)
+            email = it.getString(ARG_EMAIL)
+            password = it.getString(ARG_PASSWORD)
+            userManager = UserManager(requireContext())
         }
     }
 
@@ -33,26 +40,88 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.findViewById<TextView>(R.id.textViewName)?.text = name
+        view.findViewById<TextView>(R.id.textViewEmail)?.text = email
+        view.findViewById<TextView>(R.id.textViewPassword)?.text = password
+        view.findViewById<Button>(R.id.logoutButton)?.setOnClickListener {
+            userManager?.logOut()
+            User = userManager?.getUser()
+            if (User == null) {
+                Toast.makeText(requireContext(), "Logged out", Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
+            }
+
+        }
+
+        view.findViewById<ImageView>(R.id.editIconName)?.setOnClickListener {
+            showEditDialog("Name") { editedText ->
+                view.findViewById<TextView>(R.id.textViewName)?.text = editedText
+            }
+        }
+
+        view.findViewById<ImageView>(R.id.editIconPassword)?.setOnClickListener {
+            showEditDialog("Password") { editedText ->
+                view.findViewById<TextView>(R.id.textViewPassword)?.text = editedText
+            }
+        }
+
+        view.findViewById<ImageView>(R.id.editIconEmail)?.setOnClickListener {
+            showEditDialog("Email") { editedText ->
+                view.findViewById<TextView>(R.id.textViewEmail)?.text = editedText
+            }
+        }
+
+        return view
+    }
+
+    private fun showEditDialog(title: String, onTextEdited: (String) -> Unit) {
+        val editText = EditText(requireContext())
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle("Edit $title")
+            .setView(editText)
+            .setPositiveButton("Save", null)
+            .setNegativeButton("Cancel", null)
+            .create()
+
+        dialog.setOnShowListener {
+            val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            positiveButton.setOnClickListener {
+                val editedText = editText.text.toString().trim()
+
+                val isValidEmail = if (title.equals("Email", ignoreCase = true)) {
+                    android.util.Patterns.EMAIL_ADDRESS.matcher(editedText).matches()
+                } else {
+                    true
+                }
+
+                if (editedText.isNotEmpty() && isValidEmail) {
+                    onTextEdited.invoke(editedText)
+                    dialog.dismiss()
+                    Log.d("ProfileFragment", "Name: $name, Email: $email, Password: $password")
+                } else {
+                    val errorMessage = when {
+                        editedText.isEmpty() -> "Please enter a valid $title"
+                        !isValidEmail -> "Please enter a valid email address"
+                        else -> "Invalid input"
+                    }
+                    Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        dialog.show()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(name: String, email: String, password: String) =
             ProfileFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_NAME, name)
+                    putString(ARG_EMAIL, email)
+                    putString(ARG_PASSWORD, password)
                 }
             }
     }
--- frontend/app/src/main/java/pl/edu/agh/utp/model/User.kt
+++ frontend/app/src/main/java/pl/edu/agh/utp/model/User.kt
@@ -4,4 +4,6 @@ data class User(
     val id: Long,
     val name: String,
     val email: String
+
+
 )
